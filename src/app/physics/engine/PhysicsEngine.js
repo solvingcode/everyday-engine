d7ca944e75f10d069139ad6d8653eba8e4d134f8
solvingcode <solvingame@gmail.com>
2021-05-23 01:42:01 -0400

    day 137

--- src/app/physics/engine/PhysicsEngine.js
+++ src/app/physics/engine/PhysicsEngine.js
@@ -1,194 +1,125 @@
-import PhysicsEngineData from '../../project/data/PhysicsEngineData.js'
 import SystemError from '../../exception/type/SystemError.js'
+import {PrimitiveShape} from '../../unit/Unit.js'
+import RectColliderLoader from './loader/RectColliderLoader.js'
 
 /**
- * Used as interface between the application and engine (third party Engine)
  * @abstract
  */
-class PhysicsEngine extends PhysicsEngineData {
+export default class PhysicsEngine {
+
+    /**
+     * @type {{unitId: number, rigidBody: *}[]}
+     */
+    mapRigidBodyUnit
+
+    /**
+     * @type {Matter}
+     */
+    instance
 
     constructor() {
-        super()
-        this.mapShapeToUnit = []
+        this.mapRigidBodyUnit = []
     }
 
-    /**
-     * @abstract
-     */
-    init() {
-        throw new SystemError(`${this.constructor.name}.init method must be implemented`)
+    init(){
+        this.instance = this.createEngineInstance()
     }
 
     /**
      * @param {Unit} unit
-     * @param {World} world
      */
-    add(unit, world) {
-        this.mapShapeToUnit.push({
-            unitId: unit.getId(),
-            shape: this.loadShape(unit, world)
-        })
+    addUnit(unit) {
+        if(!this.findRigidBody(unit)){
+            this.mapRigidBodyUnit.push({
+                unitId: unit.getId(),
+                rigidBody: this.loadRigidBody(unit)
+            })
+        }
     }
 
     /**
-     * @abstract
      * @param {Unit} unit
-     * @param {World} world
      * @return {*}
      */
-    loadShape(unit, world) {
-        throw new SystemError(`${this.constructor.name}.loadShape method must be implemented`)
+    loadRigidBody(unit) {
+        const colliderBodies = [].map(colliderComponent =>
+            this.getColliderLoader(colliderComponent).load(unit, colliderComponent))
+        const rigidBody = this.newRigidBody(colliderBodies)
+        this.addToWorld(rigidBody)
+        return rigidBody
     }
 
     /**
-     * @abstract
-     * @param {Unit} unit
+     * @param {Component} component
+     * @return {ColliderLoader}
      */
-    update(unit) {
-        throw new SystemError(`${this.constructor.name}.update method must be implemented`)
+    getColliderLoader(component){
+        let shape = component.getShape()
+        switch(shape){
+            case PrimitiveShape.RECT:
+                return RectColliderLoader
+            default:
+                throw new SystemError(`No ColliderLoader configured for "${shape}"`)
+        }
+    }
+
+    /**
+     * @param {Unit} unit
+     * @return {Object}
+     */
+    findRigidBody(unit){
+        const mapRigidBody = this.mapRigidBodyUnit.find(bodyUnit => bodyUnit.unitId === unit.getId())
+        return mapRigidBody && mapRigidBody.rigidBody
     }
 
     /**
      * @abstract
-     * Get the Engine (third party)
+     * @param {*[]} colliderBodies
      * @return {*}
      */
-    getEngine() {
+    newRigidBody(colliderBodies){
+        throw new SystemError(`${this.constructor.name}.newRigidBody method must be implemented`)
+    }
+
+    /**
+     * @abstract
+     * @param {*} rigidBody
+     */
+    addToWorld(rigidBody){
+        throw new SystemError(`${this.constructor.name}.addToWorld method must be implemented`)
+    }
+
+    /**
+     * @abstract
+     * @return {*}
+     */
+    getEngine(unit) {
         throw new SystemError(`${this.constructor.name}.getEngine method must be implemented`)
     }
 
     /**
      * @abstract
-     * Run the physics engine.
-     */
-    run() {
-        throw new SystemError(`${this.constructor.name}.run method must be implemented`)
-    }
-
-    /**
-     * Stop the physics engine and reset the mapping.
-     */
-    stop() {
-        this.stopEngine()
-        this.mapShapeToUnit = []
-    }
-
-    /**
-     * @abstract
-     */
-    stopEngine() {
-        throw new SystemError(`${this.constructor.name}.stopEngine method must be implemented`)
-    }
-
-    /**
-     * @abstract
-     * Update the physics engine.
-     */
-    updateEngine() {
-        throw new SystemError(`${this.constructor.name}.updateEngine method must be implemented`)
-    }
-
-    /**
-     * @abstract
-     * Get bodies information (position, ...)
-     * @return {*[]}
-     */
-    getBodies() {
-        throw new SystemError(`${this.constructor.name}.getBodies method must be implemented`)
-    }
-
-    /**
-     * @abstract
-     * Get joints information (position, ...)
-     * @return {*[]}
-     */
-    getJoints() {
-        throw new SystemError(`${this.constructor.name}.getJoints method must be implemented`)
-    }
-
-    /**
-     * @abstract
-     * Create new group of collision
      * @return {*}
      */
-    newGroup() {
-        throw new SystemError(`${this.constructor.name}.newGroup method must be implemented`)
+    createEngineInstance(){
+        throw new SystemError(`${this.constructor.name}.createEngineInstance method must be implemented`)
     }
 
     /**
      * @abstract
-     * @param {Unit} unit
      */
-    updateCollisionFilters(unit) {
-        throw new SystemError(`${this.constructor.name}.updateCollisionFilters method must be implemented`)
+    update(){
+        throw new SystemError(`${this.constructor.name}.update method must be implemented`)
     }
 
     /**
-     * @abstract
-     * Update constraint bodies using the given constraint param
-     * @param {Unit} unit
-     * @param {Constraint} constraint
+     * @return {*}
      */
-    updateConstraint(unit, constraint) {
-        throw new SystemError(`${this.constructor.name}.updateConstraint method must be implemented`)
-    }
-
-    /**
-     * @abstract
-     * Apply force to the entity
-     * @param {Object} body
-     * @param {EntityMotion} entity
-     */
-    applyPhysics(body, entity) {
-        throw new TypeError('"applyForce" method must be implemented')
-    }
-
-    /**
-     * @param {Unit} unit
-     * @abstract
-     */
-    removeShape(unit) {
-        throw new SystemError(`${this.constructor.name}.removeShape method must be implemented`)
-    }
-
-    /**
-     * Check if two entity collide
-     * @abstract
-     * @param {Number} unitAId
-     * @param {Number} unitBId
-     */
-    isCollide(unitAId, unitBId) {
-        throw new SystemError(`${this.constructor.name}.isCollide method must be implemented`)
-    }
-
-    /**
-     * Get body physics from the entity
-     * @param {Entity} entity
-     */
-    getBodyFromEntity(entity) {
-        return this.physicsManager.getBodyFromEntity(entity)
-    }
-
-    /**
-     * Find the shape from the entity
-     * @param {Unit} unit
-     */
-    findShapeFromUnit(unit) {
-        const shapeUnit = this.mapShapeToUnit
-            .find(mShape => mShape.unitId === unit.getId())
-        return shapeUnit && shapeUnit.shape
-    }
-
-    /**
-     * @abstract
-     * @param {World} world
-     * @param {Entity} entity
-     * @return {boolean}
-     */
-    updateJointPosition(world, entity) {
-        throw new SystemError(`${this.constructor.name}.updateJointPosition method must be implemented`)
+    getInstance(){
+        if(!this.instance){
+            throw new SystemError(`Physics Engine "${this.constructor.name}" not initialized`)
+        }
+        return this.instance
     }
 
 }
-
-export default PhysicsEngine
--- src/app/physics/engine/matter/MatterEngine.js
+++ src/app/physics/engine/matter/MatterEngine.js
@@ -1,175 +1,47 @@
 import PhysicsEngine from '../PhysicsEngine.js'
-import ShapeLoader from '../ShapeLoader.js'
-import Vector from '../../../utils/Vector.js'
 
-/**
- * Matter Physics Engine class
- * @extends {PhysicsEngine}
- */
-class MatterEngine extends PhysicsEngine {
-
-    constructor() {
-        super()
-    }
+export default class MatterEngine extends PhysicsEngine {
 
     /**
      * @override
+     * @param {Unit} unit
+     * @return {Matter}
      */
-    init() {
-        this.engine = Matter.Engine.create()
-        this.shapeLoader = new ShapeLoader(this)
-    }
-
-    /**
-     * @override
-     */
-    loadShape(entity, world) {
-        const shape = this.shapeLoader.load(entity, world)
-        Matter.World.add(this.engine.world, shape)
-        return shape
-    }
-
-    /**
-     * @override
-     */
-    update(entity) {
-        this.shapeLoader.update(entity)
-    }
-
-    /**
-     * Run the physics engine
-     */
-    run() {
-        //Matter.Engine.run(this.engine)
-    }
-
-    /**
-     * Stop the physics engine
-     */
-    stopEngine() {
-        Matter.World.clear(this.engine.world)
-        Matter.Engine.clear(this.engine)
-        this.engine = null
-    }
-
-    /**
-     * @override
-     */
-    updateEngine() {
-        Matter.Engine.update(this.engine)
-    }
-
-    /**
-     * @override
-     */
-    getEngine() {
+    getEngine(unit) {
         return Matter
     }
 
     /**
      * @override
+     * @param {Matter.Body} rigidBody
      */
-    getBodies() {
-        return Matter.Composite.allBodies(this.engine.world)
+    addToWorld(rigidBody) {
+        Matter.World.add(this.getInstance().world, rigidBody)
+    }
+
+    /**
+     * @override
+     * @param {Matter.Body[]} colliderBodies
+     * @return {Matter.Body}
+     */
+    newRigidBody(colliderBodies) {
+        return Matter.Body.create({
+            parts: colliderBodies
+        })
+    }
+
+    /**
+     * @override
+     * @return {Matter}
+     */
+    createEngineInstance() {
+        return Matter.Engine.create()
     }
 
     /**
      * @override
      */
-    getJoints() {
-        return Matter.Composite.allConstraints(this.engine.world)
+    update(){
+        Matter.Engine.update(this.getInstance())
     }
-
-    /**
-     * @override
-     */
-    newGroup() {
-        return Matter.ContentMenuItem.nextGroup(true)
-    }
-
-    /**
-     * @override
-     */
-    updateCollisionFilters(entity) {
-        const body = this.getBodyFromEntity(entity)
-        body.collisionFilter = entity.collision
-        return true
-    }
-
-    /**
-     * @override
-     */
-    updateJointPosition(world, entity) {
-        const {vertices} = entity
-        const pointA = entity.toAbsolutePosition(vertices[0])
-        const pointB = entity.toAbsolutePosition(vertices[1])
-        if (entity.attached) {
-            const entityA = entity.getLinkedEntityAt(0, world)
-            entityA && entityA.movePointTo(pointA, pointB)
-            entity.movePointTo(pointA, pointB)
-            entity.updatePoints(pointB, new Vector({x: pointB.x + 1, y: pointB.y + 1}))
-            return true
-        }
-        return false
-    }
-
-    /**
-     * @override
-     */
-    updateConstraint(entity, constraint) {
-        const {entityB, pointA, pointB} = constraint
-        const body = this.getBodyFromEntity(entity)
-        const bodyB = entityB && this.getBodyFromEntity(entityB)
-        if (entityB && !bodyB) {
-            throw new TypeError(`updateConstraint failed! Body not found for entity ${entity.id}`)
-        }
-        body.bodyB = bodyB
-        body.angleB = bodyB && bodyB.angle
-        body.pointA = pointA
-        body.pointB = pointB
-    }
-
-    /**
-     * @override
-     * @todo Implement an intelligent controlling physics
-     */
-    applyPhysics(body, entity) {
-        const {force} = entity.physics
-        if (entity.isControlled()) {
-            const moveSpeed = 2
-            this.getEngine().ContentMenuItem.setPosition(body, {x: body.position.x + moveSpeed, y: body.position.y})
-        } else {
-            this.getEngine().ContentMenuItem.applyForce(body, entity.getForcePosition(), force)
-        }
-    }
-
-    /**
-     * @override
-     */
-    removeShape(entity) {
-        const body = this.getBodyFromEntity(entity)
-        this.getEngine().World.remove(this.engine.world, body)
-    }
-
-    /**
-     * @override
-     */
-    isCollide(entityAId, entityBId) {
-        const physicsManager = this.getPhysicsManager()
-        const entityA = physicsManager.getEntityById(entityAId)
-        const entityB = physicsManager.getEntityById(entityBId)
-        if (!entityA || !entityB) {
-            throw new TypeError(`Cannot check collision - entity not founded (A: ${!!entityA}, B: ${!!entityB})`)
-        }
-        const bodyA = this.getBodyFromEntity(entityA)
-        const bodyB = this.getBodyFromEntity(entityB)
-        if (!bodyA || !bodyB) {
-            throw new TypeError(`Cannot check collision - body not founded (A: ${!!bodyA}, B: ${!!bodyB})`)
-        }
-        const collision = Matter.SAT.collides(bodyA, bodyB)
-        return collision.collided
-    }
-
 }
-
-export default MatterEngine
