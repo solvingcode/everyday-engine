54a03aae78d5b9841ee7031e7fb3ef7779c8d45e
solvingcode <solvingame@gmail.com>
2021-07-31 00:55:07 -0400

    day 171

--- src/app/runner/action/form/FormUpdateAction.js
+++ src/app/runner/action/form/FormUpdateAction.js
@@ -16,8 +16,11 @@ class FormUpdateAction extends Action {
      * @override
      */
     static run() {
-        const {item} = StateManager.get().getNextProgressData(this.STATE)
+        const {event, item} = StateManager.get().getNextProgressData(this.STATE)
         item.setIsEditing(true)
+        if(this.isAtChangeField(item.field) && this.isFieldChanged(item)){
+            this.save(event, item)
+        }
         return this.isInstantField(item.field)
     }
 
@@ -26,20 +29,28 @@ class FormUpdateAction extends Action {
      */
     static stop() {
         const {event, item} = StateManager.get().getNextStopData(this.STATE)
+        this.save(event, item)
+        item.setIsEditing(false)
+        return true
+    }
+
+    /**
+     * @param {Function} event
+     * @param {MenuItem}item
+     */
+    static save(event, item){
         const menu = Menu.get()
         const menuItemUI = menu.findItemByElement(item)
         const uiRenderer = menu.getUIRenderer()
-        item.setIsEditing(false)
         if (menuItemUI) {
             const formElement = uiRenderer.getType(menuItemUI).getFormElement(menuItemUI, uiRenderer)
-            if(item.parent.preUpdate(formElement.value)){
+            if (item.parent.preUpdate(formElement.value)) {
                 event(formElement.value)
                 item.parent.postUpdate(formElement.value)
             }
         } else {
             throw new SystemError(`ElementUI for item ${item.id} cannot be found!`)
         }
-        return true
     }
 
     /**
@@ -53,11 +64,36 @@ class FormUpdateAction extends Action {
             Layout.form.TEXT,
             Layout.form.TEXTAREA,
             Layout.form.COLOR,
+            Layout.form.RANGE,
             Layout.form.FILE,
             Layout.form.WYSIWYG
         ].includes(field)
     }
 
+    /**
+     * Check if the update event must be triggered when the field's value is changed
+     * @param {string} field
+     * @return {boolean}
+     */
+    static isAtChangeField(field) {
+        return [
+            Layout.form.COLOR, Layout.form.RANGE
+        ].includes(field)
+    }
+
+    /**
+     * @param {MenuItem} item
+     * @return {boolean}
+     */
+    static isFieldChanged(item) {
+        const menu = Menu.get()
+        const menuItemUI = menu.findItemByElement(item)
+        const uiRenderer = menu.getUIRenderer()
+        const formElement = uiRenderer.getType(menuItemUI).getFormElement(menuItemUI, uiRenderer)
+        const oldValue = item.value()
+        const newValue = formElement.value
+        return oldValue !== newValue
+    }
 }
 
 export default FormUpdateAction
