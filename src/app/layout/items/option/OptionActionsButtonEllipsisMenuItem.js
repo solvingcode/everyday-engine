45f9e5f8f3fc64c05d243401c7f128b1280eed91
solvingcode <solvingame@gmail.com>
2021-08-15 01:14:05 -0400

    day 183

--- src/app/layout/items/option/OptionActionsButtonEllipsisMenuItem.js
+++ src/app/layout/items/option/OptionActionsButtonEllipsisMenuItem.js
@@ -1,23 +1,21 @@
 import MenuItem from '../../MenuItem.js'
 import Layout from '../../Layout.js'
 
-export default class OptionActionsButtonMenuItem extends MenuItem {
+export default class OptionActionsButtonEllipsisMenuItem extends MenuItem {
 
     /**
-     * @param {string} name
      * @param {OptionActionsMenuItem} optionActionsMenuItem
      * @param {*} object
-     * @param {boolean} hasParent
      */
-    constructor(name, optionActionsMenuItem, object = null, hasParent = false) {
+    constructor(optionActionsMenuItem, object = null) {
         super({
-            name,
+            name: 'ellipsis-v',
+            title: 'Options',
             stateCode: 'ACTION_OPEN_OPTION',
-            type: Layout.type.BUTTON,
+            type: Layout.type.ICON,
             zone: Layout.zone.RIGHT
         })
         this.data = {optionActionsMenuItem, object}
-        this.hasParent = hasParent
     }
 
 }
